fe76531b5b272dd1af2e59be00a9f43119518374
Robert Crocker <rcrocker13@gmail.com>
2024-06-29 15:00:45 -0500

    'Updated project to a Time Tracking Dashboard solution, includes new images/icons for UI.'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Report for Jeremy Robson Daily Weekly Monthly Work 5hrs{" "}
+      {/*<!-- daily --> */}
+      Previous - 7hrs {/*<!-- daily --> */}
+      32hrs {/*<!-- weekly --> */}
+      Previous - 36hrs {/*<!-- weekly --> */}
+      103hrs {/*<!-- monthly --> */}
+      Previous - 128hrs {/*<!-- monthly --> */}
+      Play 1hr {/*<!-- daily --> */}
+      Previous - 2hrs {/*<!-- daily --> */}
+      10hrs {/*<!-- weekly --> */}
+      Previous - 8hrs {/*<!-- weekly --> */}
+      23hrs {/*<!-- monthly --> */}
+      Previous - 29hrs {/*<!-- monthly --> */}
+      Study 0hrs {/*<!-- daily --> */}
+      Previous - 1hr {/*<!-- daily --> */}
+      4hrs {/*<!-- weekly --> */}
+      Previous - 7hrs {/*<!-- weekly --> */}
+      13hrs {/*<!-- monthly --> */}
+      Previous - 19hrs {/*<!-- monthly --> */}
+      Exercise 1hr {/*<!-- daily --> */}
+      Previous - 1hr {/*<!-- daily --> */}
+      4hrs {/*<!-- weekly --> */}
+      Previous - 5hrs {/*<!-- weekly --> */}
+      11hrs {/*<!-- monthly --> */}
+      Previous - 18hrs {/*<!-- monthly --> */}
+      Social 1hr {/*<!-- daily --> */}
+      Previous - 3hrs {/*<!-- daily --> */}
+      5hrs {/*<!-- weekly --> */}
+      Previous - 10hrs {/*<!-- weekly --> */}
+      21hrs {/*<!-- monthly --> */}
+      Previous - 23hrs {/*<!-- monthly --> */}
+      Self Care 0hrs {/*<!-- daily --> */}
+      Previous - 1hr {/*<!-- daily --> */}
+      2hrs {/*<!-- weekly --> */}
+      Previous - 2hrs {/*<!-- weekly --> */}
+      7hrs {/*<!-- monthly --> */}
+      Previous - 11hrs {/*<!-- monthly --> */}
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
-import type { Config } from "tailwindcss"
+const defaultTheme = require("tailwindcss/defaultTheme");
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -18,7 +19,35 @@ const config = {
       },
     },
     extend: {
+      // Custom screen breakpoints
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      // Custom typography
+      fontFamily: {
+        rubik: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        body: "18px",
+      },
+      fontWeight: {
+        light: "300",
+        normal: "400",
+        medium: "500",
+      },
       colors: {
+        blue: "hsl(246, 80%, 60%)",
+        "light-red-work": "hsl(15, 100%, 70%)",
+        "soft-blue-play": "hsl(195, 74%, 62%)",
+        "light-red-study": "hsl(348, 100%, 68%)",
+        "lime-green-exercise": "hsl(145, 58%, 55%)",
+        "violet-social": "hsl(264, 64%, 52%)",
+        "soft-orange-self-care": "hsl(43, 84%, 65%)",
+        "very-dark-blue": "hsl(226, 43%, 10%)",
+        "dark-blue": "hsl(235, 46%, 20%)",
+        "desaturated-blue": "hsl(235, 45%, 61%)",
+        "pale-blue": "hsl(236, 100%, 87%)",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -75,6 +104,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
